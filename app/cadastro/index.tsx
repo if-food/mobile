d42884e901e6914bce212e10bc86c7567e77ece3
thiago <1thiago1henrique@gmail.com>
2024-07-28 20:38:42 -0300

    feat: tentativa de implementacao axios

--- app/cadastro/index.tsx
+++ app/cadastro/index.tsx
@@ -1,11 +1,40 @@
 import CustomInput from 'components/customInput';
-import React, { useState } from 'react';
-import { View, Text, Form, Input, Image, Button } from 'tamagui';
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'tamagui';
 import ButtonCustom from './../../components/ButtonCustom/index';
 import { useRouter } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Pressable, TextInput } from 'react-native';
+import axios from 'axios';
+
+
+interface CadastroInterface {
+  nome: string;
+  email: string;
+  senha: string;
+}
 
 export default function Cadastro() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  useEffect(() => { console.log(nome) });
+
+  function salvar() {
+    const userRequest = {
+      nome: nome,
+      email: email,
+      senha: senha,
+      enderecos: []
+    }
+    console.log(JSON.stringify(userRequest));
+    axios.post('http://localhost:8080/api/usuario', userRequest)
+    .then((response) => {
+        console.log('usuario criado com sucesso');
+      }
+    ).catch((error) => {
+      console.log('erro ao criar usuario ' + error.response);
+    });
+  }
 
   const router = useRouter();
 
@@ -13,26 +42,34 @@ export default function Cadastro() {
     router.push('../login');
   };
 
-
   return (
     <View className="flex-1 justify-between items-center bg-[#2c2d33] pt-[24px]">
       <View>
-        <CustomInput titleInput="Nome" placeholder="Insira seu nome" />
-        <CustomInput titleInput="E-mail" placeholder="Insira seu E-mail" />
-        <CustomInput titleInput="Senha" placeholder="Insira sua senha" secureTextEntry />
-        <CustomInput titleInput="Confirmação senha" placeholder="Insira novamente sua senha" secureTextEntry />
+        <Text>Nome</Text>
+        <TextInput placeholder='Insira seu nome' value={nome} onChangeText={(input) => setNome(input)} className='w-[320px] h-[48px] bg-[#E3E3E3] text-[#16161d] px-2 rounded-[8px]'/>
       </View>
+
+      <View>
+        <Text>Email</Text>
+        <TextInput placeholder='Insira seu email' value={email} onChangeText={(input) => setEmail(input)} className='w-[320px] h-[48px] bg-[#E3E3E3] text-[#16161d] px-2 rounded-[8px]'/>
+      </View>
+
+      <View>
+        <Text>Nome</Text>
+        <TextInput placeholder='Insira sua senha' value={senha} onChangeText={(input) => setSenha(input)} className='w-[320px] h-[48px] bg-[#E3E3E3] text-[#16161d] px-2 rounded-[8px]'/>
+      </View>
+
 
       <View className="items-center">
         <View className="flex-row items-center">
-          <Text className="text-[#fff] font-bold">Lembra sua senha? {' '}</Text>
-          
+          <Text className="text-[#fff] font-bold">Lembra sua senha? </Text>
+
           <Pressable onPress={irParaLogin} className="py-6">
             <Text className="text-[#fff] font-thin underline">Faça login</Text>
           </Pressable>
         </View>
         <ButtonCustom texto="Continuar com o Google" />
-        <ButtonCustom texto="Continuar" />
+        <ButtonCustom onPress={salvar} texto="Continuar" />
       </View>
     </View>
   );
--- components/customInput/index.tsx
+++ components/customInput/index.tsx
@@ -1,19 +1,48 @@
-import { Input, Text, View } from 'tamagui';
+import React, { useState } from 'react';
+import { TextInput } from 'react-native';
+import { Text, View } from 'tamagui';
 
 interface InputCustom {
   placeholder?: string;
   titleInput?: string;
   subtitleInput?: string;
   secureTextEntry?: boolean;
+  value?: string; 
+  onChangeText?: (text: string) => void; 
+  onBlur?: () => void;
 }
 
-const CustomInput = ({placeholder = '', titleInput = '', subtitleInput = '', secureTextEntry = false}: InputCustom) => {
+const CustomInput = ({
+  placeholder = '',
+  titleInput = '',
+  subtitleInput = '',
+  secureTextEntry = false,
+  value = '', 
+  onChangeText = (text: string) => {},
+  onBlur = () => {} 
+}: InputCustom) => {
+  const [internalValue, setInternalValue] = useState(value);
+
+  const handleChangeText = (text: string) => {
+    console.log(`Text changed: ${text}`); 
+    setInternalValue(text);
+  };
 
   return (
     <View className='pb-6'>
       {titleInput ? <Text className='text-[16px] font-bold text-[#fff] pb-2'>{titleInput}</Text> : null}
       {subtitleInput ? <Text className='text-[12px] font-thin text-[#fff] pb-2'>{subtitleInput}</Text> : null}
-      <Input secureTextEntry={secureTextEntry} placeholder={placeholder} className='w-[320px] h-[48px] bg-[#E3E3E3] text-[#16161d]'/>
+      <TextInput
+        onChangeText={handleChangeText}
+        onBlur={() => {
+          console.log(`Input blurred with value: ${internalValue}`); 
+          onChangeText(internalValue);
+        }}
+        secureTextEntry={secureTextEntry}
+        value={internalValue}
+        placeholder={placeholder}
+        className='w-[320px] h-[48px] bg-[#E3E3E3] text-[#16161d] px-2 rouned'
+      />
     </View>
   );
 };
