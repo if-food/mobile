0c48e6732533265b273c743898dee3627bd9e246
Carlos Gabriel <carlosgmn31@gmail.com>
2024-08-03 21:05:03 -0300

    fix intancia axios e contruindo a primeira requisição

--- app/cadastro/index.tsx
+++ app/cadastro/index.tsx
@@ -1,11 +1,24 @@
 import CustomInput from 'components/customInput';
-import React, { useState } from 'react';
-import { View, Text, Form, Input, Image, Button } from 'tamagui';
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'tamagui';
 import ButtonCustom from './../../components/ButtonCustom/index';
 import { useRouter } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Pressable, TextInput } from 'react-native';
+import { singInClient } from 'app/services/AuthenticateService';
+
+
+
+interface CadastroInterface {
+  nome: string;
+  email: string;
+  senha: string;
+}
 
 export default function Cadastro() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  useEffect(() => { console.log(nome) });
 
   const router = useRouter();
 
@@ -13,27 +26,55 @@ export default function Cadastro() {
     router.push('../login');
   };
 
+  const irParaHome = () => {
+    router.push('../home');
+  };
+  
+  async function Cadastrar() {
+    console.log(email, senha) 
+    const response = singInClient(email, senha)
+    if (response !== undefined && response !== null) {
+      alert('Usuario criado com sucesso')
+      irParaHome()
+    }
+  }
 
-  return (
-    <View className="flex-1 justify-between items-center bg-[#2c2d33] pt-[24px]">
-      <View>
-        <CustomInput titleInput="Nome" placeholder="Insira seu nome" />
-        <CustomInput titleInput="E-mail" placeholder="Insira seu E-mail" />
-        <CustomInput titleInput="Senha" placeholder="Insira sua senha" secureTextEntry />
-        <CustomInput titleInput="Confirmação senha" placeholder="Insira novamente sua senha" secureTextEntry />
-      </View>
 
-      <View className="items-center">
-        <View className="flex-row items-center">
-          <Text className="text-[#fff] font-bold">Lembra sua senha? {' '}</Text>
-          
-          <Pressable onPress={irParaLogin} className="py-6">
-            <Text className="text-[#fff] font-thin underline">Faça login</Text>
-          </Pressable>
+
+
+    return (
+      <View className="flex-1 justify-between items-center bg-[#2c2d33] pt-[24px]">
+        <View>
+          <Text>Nome</Text>
+          <TextInput placeholder='Insira seu nome' value={nome} onChangeText={(input) => setNome(input)} className='w-[320px] h-[48px] bg-[#E3E3E3] text-[#16161d] px-2 rounded-[8px]' />
         </View>
-        <ButtonCustom texto="Continuar com o Google" />
-        <ButtonCustom texto="Continuar" />
+
+        <View>
+          <Text>Email</Text>
+          <TextInput placeholder='Insira seu email' value={email} onChangeText={(input) => setEmail(input)} className='w-[320px] h-[48px] bg-[#E3E3E3] text-[#16161d] px-2 rounded-[8px]' />
+        </View>
+
+        <View>
+          <Text>Senha</Text>
+          <TextInput placeholder='Insira sua senha' value={senha} onChangeText={(input) => setSenha(input)} className='w-[320px] h-[48px] bg-[#E3E3E3] text-[#16161d] px-2 rounded-[8px]' />
+        </View>
+        <View>
+          <Text>Confirmação</Text>
+          <TextInput placeholder='digite novamente sua senha' className='w-[320px] h-[48px] bg-[#E3E3E3] text-[#16161d] px-2 rounded-[8px]' />
+        </View>
+
+
+        <View className="items-center">
+          <View className="flex-row items-center">
+            <Text className="text-[#fff] font-bold">Lembra sua senha? </Text>
+
+            <Pressable onPress={irParaLogin} className="py-6">
+              <Text className="text-[#fff] font-thin underline">Faça login</Text>
+            </Pressable>
+          </View>
+          <ButtonCustom texto="Continuar com o Google" />
+          <ButtonCustom onPress={Cadastrar} texto="Continuar" />
+        </View>
       </View>
-    </View>
-  );
-}
+    );
+  }
